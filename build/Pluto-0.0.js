2185021642b1fe05a43a14630da15593e9e6be17
Ruby Fumizki <ruby@RubynoMacBook-Pro.local>
2020-04-12 09:49:47 +0900

    ver0.0

--- build/Pluto-0.0.js
+++ build/Pluto-0.0.js
@@ -1,69 +1,20 @@
 /*
-Copyright (C) 2019 Ruby Fumizki
+Pluto.js ver0.0
+Pluto.js is ephemeris calculator for Sun, Moon and Planets.
 
-This is released under the GNU GPLv3.
-This fale is made from the Swiss Ephemeris Free Edition, Version 2.00.00 of Astrodienst AG, Switzerland from the original C Code to JavaScript.
+Licensed under GNU General Public License v2.0
 
-For any questions or comments regarding this file, you should contact me.
-
-March 1, 2019
-Ruby Fumizki
-http://rubyfmzk.com
+Copyright 2020- Ruby Fumizuki
+https://rubyfmzk.com
+https://github.com/rubyfmzk/Y2J
 rubyfmzk@gmail.com
 */
+/* 
+This implementation based on code by Steve Moshier.
+http://www.moshier.net/
 
-/* Copyright (C) 1997 - 2008 Astrodienst AG, Switzerland.  All rights reserved.
-
-  License conditions
-  ------------------
-
-  This file is part of Swiss Ephemeris.
-
-  Swiss Ephemeris is distributed with NO WARRANTY OF ANY KIND.  No author
-  or distributor accepts any responsibility for the consequences of using it,
-  or for whether it serves any particular purpose or works at all, unless he
-  or she says so in writing.
-
-  Swiss Ephemeris is made available by its authors under a dual licensing
-  system. The software developer, who uses any part of Swiss Ephemeris
-  in his or her software, must choose between one of the two license models,
-  which are
-  a) GNU public license version 2 or later
-  b) Swiss Ephemeris Professional License
-
-  The choice must be made before the software developer distributes software
-  containing parts of Swiss Ephemeris to others, and before any public
-  service using the developed software is activated.
-
-  If the developer choses the GNU GPL software license, he or she must fulfill
-  the conditions of that license, which includes the obligation to place his
-  or her whole software project under the GNU GPL or a compatible license.
-  See http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-
-  If the developer choses the Swiss Ephemeris Professional license,
-  he must follow the instructions as found in http://www.astro.com/swisseph/
-  and purchase the Swiss Ephemeris Professional Edition from Astrodienst
-  and sign the corresponding license contract.
-
-  The License grants you the right to use, copy, modify and redistribute
-  Swiss Ephemeris, but only under certain conditions described in the License.
-  Among other things, the License requires that the copyright notices and
-  this notice be preserved on all copies.
-
-  Authors of the Swiss Ephemeris: Dieter Koch and Alois Treindl
-
-  The authors of Swiss Ephemeris have no control or influence over any of
-  the derived works, i.e. over software or services created by other
-  programmers which use Swiss Ephemeris functions.
-
-  The names of the authors or of the copyright holder (Astrodienst) must not
-  be used for promoting any software, product or service which uses or contains
-  the Swiss Ephemeris. This copyright notice is the ONLY place where the
-  names of the authors can legally appear, except in cases where they have
-  given special permission in writing.
-
-  The trademarks 'Swiss Ephemeris' and 'Swiss Ephemeris inside' may be used
-  for promoting such software, products or services.
+The inital Javascript code has been written by mivion.
+https://github.com/mivion/ephemeris
 */
 /**
  * Ephemeris namespace
@@ -120,98 +71,6 @@ $ns.assert = function (variable, value) {
 	}
 };
 
-
-$ns.load = function () {
-    var curDate = new Date ();
-	var dateArea = document.getElementById ('$const.date');
-    dateArea.value = curDate.getDate () + '.' + (curDate.getMonth () + 1) + '.' + curDate.getFullYear () + ' ' +
-        curDate.getHours () + ':' + curDate.getMinutes () + ':' + curDate.getSeconds ()
-    ;
-
-    $e.update ();
-};
-
-$ns.update = function () {
-	var textAreas = document.body.getElementsByTagName ('textarea');
-	var selects = document.body.getElementsByTagName ('select');
-	var classes, ids, value, date;
-	var i, j, key;
-
-	//$processor.test ();
-
-	// fill input
-	if (textAreas) {
-		for (i = 0; i < textAreas.length; i ++) {
-			ids = textAreas [i].getAttribute ('id');
-			try {
-				eval ('' + ids + ' = "' + textAreas [i].value + '"');
-			} catch (exception) {
-			}
-		}
-	}
-
-	if ($const.date) {
-		var tokens = $const.date.split (' ');
-
-		tokens [0] = tokens [0].split ('.');
-		tokens [1] = tokens [1].split (':');
-
-		date = {
-			day: parseFloat (tokens [0][0]),
-			month: parseFloat (tokens [0][1]),
-			year: parseFloat (tokens [0][2]),
-			hours: parseFloat (tokens [1][0]),
-			minutes: parseFloat (tokens [1][1]),
-			seconds: parseFloat (tokens [1][2])
-		};
-		$const.date = date;
-	}
-
-	$processor.init ();
-
-	// fill input bodies
-	if (selects) {
-		for (i = 0; i < selects.length; i ++) {
-			classes = selects [i].getAttribute ('class');
-			ids = selects [i].getAttribute ('id');
-			if (classes) {
-				try {
-					var selector = eval ('(' + classes + ')');
-					if (!selects [i].innerHTML) {
-						var selections = [];
-						for (key in selector) {
-							if (selector.hasOwnProperty (key) && selector [key].key == key && key != 'earth') {
-								selections.push ('<option label=' + key + '>' + key + '</option>');
-							}
-						}
-						selects [i].innerHTML = selections;
-					}
-					eval (ids + ' = ' + classes + '.' + selects [i].value);
-				} catch (exception) {
-				}
-			}
-		}
-	}
-
-	$processor.calc (date, $const.body);
-
-	// fill output
-	if (textAreas) {
-		for (i = 0; i < textAreas.length; i ++) {
-			classes = (textAreas [i].getAttribute ('class') || '').split (' ');
-			for (j = 0; j < classes.length; j ++) {
-				try {
-					value = eval ('(' + classes [j] + ')');
-					if (value || value === 0) {
-						textAreas [i].value = value.join ? value.join ('\n') : value;
-						break;
-					}
-				} catch (exception) {
-				}
-			}
-		}
-	}
-};
 
 $ns = ephemeris.astronomy = {};
 
