82c0585c7d2e528d63ae5214049aee8483b197e6
Ruby Fumizki <ruby@RubynoMacBook-Pro.local>
2020-04-12 09:40:45 +0900

    moshierとして保存

--- src/operate.js
+++ src/operate.js
@@ -6,7 +6,8 @@ $pl.timezone = (function(){
   return dt.getTimezoneOffset() / -60;
 })();
 
-$pl.planet = {};
+$pl.planets = {};
+$pl.planetsAfterMinute = {};
 
 $pl.planetNames = [
   'sun',
@@ -19,8 +20,8 @@ $pl.planetNames = [
   'uranus',
   'neptune',
   'pluto',
-  'chiron',
-  'sirius',
+  //'chiron',
+  //'sirius',
 ];
 
 $pl.setTimezone = function(timezone){
@@ -33,7 +34,7 @@ $pl.setCurrentDate = function(){
 }
 
 $pl.setDate = function(year, month, day, hours, minutes, seconds, timezone){
-  if($pl.func.isNull(timezone)) timezone = $pl.timezone;
+  if($pl._func.isNull(timezone)) timezone = $pl.timezone;
 
   $const.date = {
     year: year,
@@ -57,42 +58,135 @@ $pl.setJsUtcDate = function(date){
   );
 }
 
-$pl.getUTCDate = function(){
+$pl.getUtcDate = function(){
   return $const.date;
 }
 
-$pl.getPosition = function(planet){
-  $processor.calc($const.date, $moshier.body[planet]);
-  $pl.planet[planet] = {
-    longitude: $const.body.position.apparentLongitude,
+$pl.getPositions = function(planet){
+  $pl.planets = {};
+  $pl.planetsAfterMinute = {};
+
+  //1分後のデータ作成
+  $const.date.minutes += 1;
+
+  for(var i=0; i<$pl.planetNames.length; i++){
+    var planet = $pl.planetNames[i];
+    $pl._func.getPositionAfterMinute(planet);
   }
 
-  switch (planet){
-    case 'sun':
-      break;
-    case 'moon':
-      $pl.planet[planet].latitude = $const.body.position.apparentGeocentric.latitude;
-      break;
-    default:
-      $pl.planet[planet].longitude60 = {
-        degree: $const.body.position.apparentGeocentric[3].degree,
-        minutes: $const.body.position.apparentGeocentric[3].minutes,
-        seconds: $const.body.position.apparentGeocentric[3].seconds,
-      }
+  //1分後のデータを戻す
+  $const.date.minutes -= 1;
+
+  for(var i=0; i<$pl.planetNames.length; i++){
+    var planet = $pl.planetNames[i];
+    $pl._func.getPosition(planet);
   }
-  return $pl.planet[planet];
+  return $pl.planets;
 }
 
-$pl.getAllPositions = function(planet){
-  for(var i=0; i<$pl.planets.length; i++){
-
-  }
-  $processor.calc($const.date, $moshier.body[planet]);
-}
-
-$pl.func = {
+$pl._func = {
   isNull: function(val){
     if(val === null || val === undefined) return true;
     return false;
+  },
+
+  getPosition: function(planet){
+    $processor.calc($const.date, $moshier.body[planet]);
+
+    var longitude = $const.body.position.apparentLongitude;
+    var latitude;
+
+    switch (planet){
+      case 'sun':
+        latitude = $const.body.position.equinoxEclipticLonLat[1] * ( 180 / Math.PI );
+        break;
+      case 'moon':
+        latitude = $const.body.position.geometric.latitude;
+        break;
+      case 'sirius':
+        break;
+      default:
+        latitude = $const.body.position.apparentGeocentric[1] * ( 180 / Math.PI );
+    }
+
+    var longitudeSpeed = ($pl.planetsAfterMinute[planet].longitude - longitude) * 60 * 24;
+    var latitudeSpeed = ($pl.planetsAfterMinute[planet].latitude - latitude) * 60 * 24;
+
+    switch (planet){
+      case 'sirius':
+        $pl.planets[planet] = {
+          longitude: longitude,
+          latitude: latitude,
+          longitudeSpeed: longitudeSpeed,
+          latitudeSpeed: latitudeSpeed,
+          longitude60: {
+            degree: parseInt(longitude),
+            minutes: parseInt(Math.abs(longitude) % 1 * 60),
+            seconds: Math.abs(longitude * 60) % 1 * 60,
+          },
+          longitudeSpeed60: {
+            degree: parseInt(longitudeSpeed),
+            minutes: parseInt(Math.abs(longitudeSpeed) % 1 * 60),
+            seconds: Math.abs(longitudeSpeed * 60) % 1 * 60,
+          },
+        }
+        break;
+      default:
+        $pl.planets[planet] = {
+          longitude: longitude,
+          latitude: latitude,
+          longitudeSpeed: longitudeSpeed,
+          latitudeSpeed: latitudeSpeed,
+          longitude60: {
+            degree: parseInt(longitude),
+            minutes: parseInt(Math.abs(longitude) % 1 * 60),
+            seconds: Math.abs(longitude * 60) % 1 * 60,
+          },
+          latitude60: {
+            degree: parseInt(latitude),
+            minutes: parseInt(Math.abs(latitude) % 1 * 60),
+            seconds: Math.abs(latitude * 60) % 1 * 60,
+          },
+          longitudeSpeed60: {
+            degree: parseInt(longitudeSpeed),
+            minutes: parseInt(Math.abs(longitudeSpeed) % 1 * 60),
+            seconds: Math.abs(longitudeSpeed * 60) % 1 * 60,
+          },
+          latitudeSpeed60: {
+            degree: parseInt(latitudeSpeed),
+            minutes: parseInt(Math.abs(latitudeSpeed) % 1 * 60),
+            seconds: Math.abs(latitudeSpeed * 60) % 1 * 60,
+          }
+        }
+    }
+
+    return $pl.planets[planet];
+  },
+
+  getPositionAfterMinute: function(planet){
+    $processor.calc($const.date, $moshier.body[planet]);
+
+    var longitude = $const.body.position.apparentLongitude;
+    var latitude;
+
+    switch (planet){
+      case 'sun':
+        latitude = $const.body.position.equinoxEclipticLonLat[1] * ( 180 / Math.PI );
+        break;
+      case 'moon':
+        latitude = $const.body.position.geometric.latitude;
+        break;
+      case 'sirius':
+        break;
+      default:
+        latitude = $const.body.position.apparentGeocentric[1] * ( 180 / Math.PI );
+    }
+
+    $pl.planetsAfterMinute[planet] = {
+      longitude: longitude,
+      latitude: latitude,
+    }
+
+    return $pl.planetsAfterMinute[planet];
   },
 }
