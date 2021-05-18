afb4bf23ea9ce5e0ffa0833142fec8f2fb72f7f2
rubyfmzk <rubyfmzk@gmail.com>
2021-05-18 21:56:39 +0900

    add eclipse

--- test/js/index.js
+++ test/js/index.js
@@ -91,6 +91,24 @@ function calc(){
 
   $_("ayanamsha").innerHTML = $pl.getAyanamsha();
 
+  let tret = new Array(10);
+  let attr = new Array(20);
+  let backward =　0;
+  //let geopos = [148.8, 10.1, 0];
+  let geopos_ = [135, 35, 0];
+  let geopos = new Array(6);
+  //let a = $pl.swe.swe_sol_eclipse_how(2457456.58216435, $pl.iflag, geopos, attr);
+  //let z = $pl.swe.swe_sol_eclipse_when_loc($pl.getJulDay()+365.25*37+200, $pl.iflag, geopos_, tret, attr, 0);
+  //let y = $pl.swe.swe_lun_occult_when_loc($pl.getJulDay()+365.25*37+200, 4, null, $pl.iflag, geopos_, tret, attr, 0);
+  let y = $pl.swe.swe_lun_eclipse_when_loc($pl.getJulDay()+365.25*37+100, $pl.iflag, geopos_, tret, attr, 0);
+//  let a = $pl.swe.swe_sol_eclipse_when_glob($pl.getJulDay()+365.25*37+200, $pl.iflag, 0, tret, backward);
+  //let b = $pl.swe.swe_sol_eclipse_how(tret[0], $pl.iflag, geopos_, attr);
+  //let c = $pl.swe.swe_sol_eclipse_where(tret[0], $pl.iflag, geopos, attr);
+  //let a = $pl.swe.swe_fixstar('deLeo', $pl.getJulDay()+365.25*35, $pl.iflag, xx);
+
+  let pheno = $pl.swe.swe_pheno($pl.getJulDay(), 4, $pl.iflag, attr);
+
+  console.error($pl.getJulDay(), tret, attr, geopos);
   var elapsed_ms = new Date().getTime() - start_ms;
   console.log('seconds：' + elapsed_ms);
 }
