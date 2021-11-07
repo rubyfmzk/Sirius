c4b87c6db99f37800cc0d88aafb478f270b14b89
rubyfmzk <rubyfmzk@gmail.com>
2021-11-08 01:13:27 +0900

    add dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
 module.exports = {
   // モード値を production に設定すると最適化された状態で、
   // development に設定するとソースマップ有効でJSファイルが出力される
-  mode: 'development',
+  // mode: 'development',
+  mode: 'production',
 
   // メインとなるJavaScriptファイル（エントリーポイント）
-  entry: './ts/index.ts',
+  entry: './src/Index.js',
 
   module: {
     rules: [
@@ -23,25 +24,24 @@ module.exports = {
   // ファイルの出力設定
   output: {
     // 出力するファイル名
-    filename: "index.js",
+    filename: "Sirius.js",
 
     //  出力先のパス
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+
+    // ライブラリモード
+    library:{
+      name: 'Sirius',
+      export: 'default',
+      type: 'umd',
+    },
   },
 
-  // import 文で .ts ファイルを解決するため
-  // これを定義しないと import 文で拡張子を書く必要が生まれる。
-  // フロントエンドの開発では拡張子を省略することが多いので、
-  // 記載したほうがトラブルに巻き込まれにくい。
-  resolve: {
-    // 拡張子を配列で指定
-    extensions: [
-      '.ts', '.js',
-    ],
-  },
   devServer: {
     static: {
       directory: path.join(__dirname, 'dist'),
     },
-  }
+  },
+
+  performance: { hints: false },
 };
